23eb82e390c977c3c7b26039fd5be97671d801f8
frh229 <frh229@lehigh.edu>
2026-05-16 10:06:27 -0400

    Finished helper function for performing the effect of an attack

--- BreadBrawl.py
+++ BreadBrawl.py
@@ -3,11 +3,11 @@ from dataclasses import dataclass
 import random
 
 class Attack(Enum):
-    slash = 0
-    drain = 1
-    heal = 2
-    sprint = 3
-    block = 4
+    block = 0
+    slash = 1
+    drain = 2
+    heal = 3
+    sprint = 4
     power_up = 5
 
 class Player(Enum):
@@ -24,11 +24,6 @@ class PlayerState:
     sprint: int
     power_up: int
 
-@dataclass
-class EnvState:
-    p1: PlayerState
-    p2: PlayerState
-
 class Loaf:
     def __init__(self, flour: int, salt: int, sugar: int, attacks: set[Attack]):
         if not (0 <= flour + salt + sugar <= 6):
@@ -37,6 +32,30 @@ class Loaf:
         self.salt = 10 + salt
         self.sugar = 10 + sugar
         self.attacks = attacks
+
+def perform_attack(attack: Attack, user: Player, user_stats: Loaf, state: dict[Player, PlayerState]):
+        match attack:
+            case Attack.block:
+                state[user].blocked = 1
+
+            case Attack.slash:
+                state[user.opponent()].hp -= (1 - state[user.opponent()].blocked) * (random.randrange(-2, 2) + user_stats.salt)
+                if state[user.opponent()].hp < 0:
+                    state[user.opponent()].hp = 0
+
+            case Attack.drain:
+                damage = min(state[user.opponent()].hp, (1 - state[user.opponent()].blocked) * (random.randrange(-2, 2) + int(0.6 * user_stats.salt)))
+                state[user.opponent()].hp -= damage
+                state[user].hp += damage
+
+            case Attack.heal:
+                state[user].hp = min(state[user].hp + user_stats.flour >> 2, user_stats.flour)
+
+            case Attack.sprint:
+                state[user].sprint = 4
+
+            case Attack.power_up:
+                state[user].power_up = 4
 
 class BreadBrawl:
     def __init__(self, p1: Loaf, p2: Loaf | None = None):
@@ -47,4 +66,6 @@ class BreadBrawl:
         self.terminated = True
 
     def reset(self):
-        self.states = EnvState(PlayerState(self.players[Player.p1.value].flour, 0, 0, 0), PlayerState(self.players[Player.p2.value].flour, 0, 0, 0))
+        self.states = {Player.p1: PlayerState(self.players[Player.p1.value].flour, 0, 0, 0), Player.p2: PlayerState(self.players[Player.p2.value].flour, 0, 0, 0)}
+        return self.states
+
