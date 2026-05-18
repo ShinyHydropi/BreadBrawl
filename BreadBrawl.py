77901465f4ea75e24075c17a94c0a5432b528e70
frh229 <frh229@lehigh.edu>
2026-05-18 12:32:51 -0400

    Changed the attack method to be non-static to reduce copying

--- BreadBrawl.py
+++ BreadBrawl.py
@@ -50,30 +50,29 @@ class BreadBrawl:
     def duel_env(self, p1: Loaf, p2: Loaf):
         return self.__init__(p1, p2)
 
-    @staticmethod
-    def _perform_attack(attack: Attack, user: Player, user_stats: Loaf, state: dict[Player, PlayerState]):
+    def _perform_attack(self, attack: Attack, user: Player):
         match attack:
             case Attack.block:
-                state[user].blocked = 1
+                self.states[user].blocked = 1
 
             case Attack.slash:
-                state[user.opponent()].hp -= (1 - state[user.opponent()].blocked) * (random.randrange(-2, 2) + user_stats.salt)
-                if state[user.opponent()].hp < 0:
-                    state[user.opponent()].hp = 0
+                self.states[user.opponent()].hp -= (1 - self.states[user.opponent()].blocked) * (random.randrange(-2, 2) + self.players[user].salt)
+                if self.states[user.opponent()].hp < 0:
+                    self.states[user.opponent()].hp = 0
 
             case Attack.drain:
-                damage = min(state[user.opponent()].hp, (1 - state[user.opponent()].blocked) * (random.randrange(-2, 2) + int(0.6 * user_stats.salt)))
-                state[user.opponent()].hp -= damage
-                state[user].hp += damage
+                damage = min(self.states[user.opponent()].hp, (1 - self.states[user.opponent()].blocked) * (random.randrange(-2, 2) + int(0.6 * self.players[user].salt)))
+                self.states[user.opponent()].hp -= damage
+                self.states[user].hp += damage
 
             case Attack.heal:
-                state[user].hp = min(state[user].hp + user_stats.flour >> 2, user_stats.flour)
+                self.states[user].hp = min(self.states[user].hp + self.players[user].flour >> 2, self.players[user].flour)
 
             case Attack.sprint:
-                state[user].sprint = 4
+                self.states[user].sprint = 4
 
             case Attack.power_up:
-                state[user].power_up = 4
+                self.states[user].power_up = 4
 
     def reset(self):
         self.states = {Player.p1: PlayerState(self.players[Player.p1].flour, 0, 0, 0), Player.p2: PlayerState(self.players[Player.p2].flour, 0, 0, 0)}
@@ -98,15 +97,14 @@ class BreadBrawl:
         elif self.players[Player.p1].sugar < self.players[Player.p2].sugar:
             order.insert(0, Player.p2)
         else:
-            order = [Player.p1]
             order.insert(random.randint(0,1), Player.p2)
 
         if p1att == Attack.block: # Performs any blocks before other attacks
-            self._perform_attack(p1att, Player.p1, self.players[Player.p1], self.states)
+            self._perform_attack(p1att, Player.p1)
 
         if p2att == Attack.block:
-            self._perform_attack(p2att, Player.p2, self.players[Player.p2], self.states)
+            self._perform_attack(p2att, Player.p2)
 
         attacks = {Player.p1: p1att, Player.p2: p2att}
         for p in order:
-            self._perform_attack(attacks[p], p, self.players[p], self.states)
+            self._perform_attack(attacks[p], p)
