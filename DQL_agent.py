b9e2a60f80f7642d1acf296debd584e60fffa6d0
frh229 <frh229@lehigh.edu>
2026-07-06 14:14:24 -0400

    Finished comments, switched to L2 loss, and adjusted hyper-parameters for shorter training

--- DQL_agent.py
+++ DQL_agent.py
@@ -11,13 +11,13 @@ from tqdm import tqdm
 
 GAMMA = 0.99
 BATCH_SIZE = 32
-BUFFER_SIZE = 50000
-MIN_REPLAY_SIZE = 1000
+BUFFER_SIZE = 5000
+MIN_REPLAY_SIZE = 100
 EPSILON_START = 1.0
 EPSILON_END = 0.02
-EPSILON_DECAY = 10000
-TARGET_UPDATE_FREQUENCY = 1000
-EPISODES = 100_000
+EPSILON_DECAY = 1000
+TARGET_UPDATE_FREQUENCY = 100
+STEPS = 10000
 
 class DQNetwork(nn.Module):
     def __init__(self, loaf, state_dim, n_actions, hidden=128):
@@ -64,25 +64,35 @@ your_loaf = Loaf.random_loaf()
 
 """
 Some more objects we will need:
-env - The environment our agent is interacting with in order to train
+env - The environment our agent is interacting with in order to train (in this case BreadBrawl)
 replay_buffer - This will store the 50,000 most recent episodes so we can collect many episodes before each
     iteration of improving our agent (This also lets us use past training data while discarding the data that
     is too old)
 reward_buffer - This keeps track of how well the agent is doing
-
 """
-
 env = BreadBrawl.training_env(your_loaf)
 replay_buffer = deque(maxlen=BUFFER_SIZE)
 reward_buffer = deque([0.0], maxlen=100)
 
+"""
+With Deep-Q learning, we will use two networks, one to update every episode that represents our current best
+estimate of the Q-function, and one to keep fixed for many episodes at a time. This fixed network is used
+during policy evaluation as a stationary target for approximating the value of the next state.
+"""
 online_net = DQNetwork(your_loaf, 8, len(your_loaf.action_space()))
 target_net = DQNetwork(your_loaf, 8, len(your_loaf.action_space()))
-
 target_net.load_state_dict(online_net.state_dict())
 
+"""
+Optimizers in Pytorch handle network updates for us. Adam is very common and uses an adaptive learning rate.
+"""
 optimizer = torch.optim.Adam(online_net.parameters(), lr=5e-4)
 
+"""
+In this section, 1000 steps are played with the agent selecting only random actions. These episodes are used
+to fill the replay buffer with many episodes to sample from. Otherwise, we would be sampling the same early
+episodes many times.
+"""
 obs = env.reset()
 for _ in range(MIN_REPLAY_SIZE):
     action = your_loaf.random_attack()
@@ -95,16 +105,34 @@ for _ in range(MIN_REPLAY_SIZE):
     if done:
         obs = env.reset()
 
+"""
+Below is where the actual training happens. Here are some important functions:
+env.reset() - Resets the environment for the next episode
+env.step_1p(action) - Changes the environment by the action the agent chose (The 1p means only one agent is
+    selecting an action)
+your_loaf.action_space() - Returns a list of all the attacks your_loaf can use
+"""
 episode_reward = 0.0
 obs = env.reset()
-for step in tqdm(range(EPISODES)):
+for step in tqdm(range(STEPS)):
+    """
+    At the beginning of each step (in this environment one step is one turn of the battle), the agent selects
+    an action to take. In order to ensure we have a comprehensive approximation of our Q-function, the agent
+    must explore many states. Once we have a better approximation of our Q-function, it is more important
+    for the agent to visit actions that it knows are more valuable. To balance this tradeoff, our agent will
+    select random actions with a probability epsilon (otherwise it selects the action with the highest value).
+    Throughout our training epsilon will decay from 1.00 to 0.02.
+    """
     epsilon = np.interp(step, [0, EPSILON_DECAY], [EPSILON_START, EPSILON_END])
-
     if random.random() <= epsilon:
         action = your_loaf.random_attack()
     else:
         action = online_net.select_action(obs)
 
+    """
+    Steps will continue to be added to the replay buffer. Once the buffer exceeds 50,000 steps the oldest ones
+    are discarded.
+    """
     next_obs, _, done, reward = env.step_1p(action)
     action = your_loaf.action_space().index(action)
     transition = (obs, action, done, reward, next_obs)
@@ -116,9 +144,19 @@ for step in tqdm(range(EPISODES)):
     if done:
         obs = env.reset()
 
+        """
+        Here we are tracking how well the agent performed. Agent's HP - Opponent's HP is used as the reward
+        signal for this environment. Feel free to try a different reward function using the info from the state.
+        """
         reward_buffer.append(episode_reward)
         episode_reward = 0.0
 
+    """
+    After each step, the agent uses a sample of transitions (the action it took and its effect on the
+    environment) from the replay buffer to improve the neural network's approximation of the Q-function.
+    to train the network, the transitions are converted into tensors (arrays that can store gradients during
+    computations)
+    """
     transitions = random.sample(replay_buffer, BATCH_SIZE)
 
     obses = np.asarray([t[0] for t in transitions])
@@ -133,17 +171,26 @@ for step in tqdm(range(EPISODES)):
     dones_t = torch.as_tensor(dones, dtype=torch.int64).unsqueeze(-1)
     next_obses_t = torch.as_tensor(next_obses, dtype=torch.float32)
 
-    target_q_values = target_net(next_obses_t)
-    max_q = target_q_values.max(1, keepdim = True)[0]
-
-    targets = rewards_t + (1 - done) * GAMMA * max_q
+    """
+    To evaluate the network's accuracy we compare the agent's approximation of the Q-value with the sum of the
+    reward received and the value of the next state (multiplied by a discount factor of 0.99). This comparison
+    is performed using L2 loss (mean squared error).
+    """
+    with torch.no_grad():
+        target_q_values = target_net(next_obses_t)
+        max_q = target_q_values.max(1, keepdim = True)[0]
+        targets = rewards_t + (1 - done) * GAMMA * max_q
 
     q_values = online_net(obses_t)
 
     action_q_values = torch.gather(q_values, 1, actions_t)
 
-    loss = nn.functional.smooth_l1_loss(action_q_values, targets)
+    loss = nn.functional.mse_loss(action_q_values, targets)
 
+    """
+    With these three lines, the gradient from the previous update is cleared, a new gradient is computed, and
+    the network is updated.
+    """
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
